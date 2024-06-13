c61f5c0743ea0fda584cf6888a96e1d7c89db3ee
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2024-06-13 12:59:24 -0600

    Resume dia 1 creacion de controllers y entendimiento de auth

---
+++ src/main/java/com/project/demo/rest/category/CategoryController.java
@@ -0,0 +1,21 @@
+package com.project.demo.rest.category;
+
+import com.project.demo.logic.entity.rol.Role;
+import com.project.demo.logic.entity.rol.RoleEnum;
+import com.project.demo.logic.entity.rol.RoleRepository;
+import com.project.demo.logic.entity.user.User;
+import com.project.demo.logic.entity.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RequestMapping("/categories")
+@RestController
+public class CategoryController {
+}
---
+++ src/main/java/com/project/demo/rest/product/ProductController.java
@@ -0,0 +1,20 @@
+package com.project.demo.rest.product;
+
+
+
+
+import com.project.demo.logic.entity.user.User;
+import com.project.demo.logic.entity.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+}
